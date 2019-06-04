ebe42090cd1701b4ef1aff086a121ab3bf92d82c
annakleinhans <annakleinhans@students.berkeley.net>
2019-06-04 14:25:07 -0700

    fdghjk

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-
-/* AJAX CALL*/
-$.ajax({
-    url: "https://api.urbandictionary.com/v0/define?term={" + $("#word") + "}",
-    type: 'GET',
-    crossDomain: true,
-    dataType: 'json',
-    success: running,
-    failure: function(){
-        alert("Error!");
-    }
-});
-
 //https://github.com/zdict/zdict/wiki/Urban-dictionary-API-documentation
 
+
+$(document).ready(function(){
+    $("#search").on("click", function(){
+
+        var palabra = $("#word").val();
+        console.log(palabra);
+
+        $.ajax({
+            url: "https://api.urbandictionary.com/v0/define?term={" + palabra + "}",
+            type: 'GET',
+            crossDomain: true,
+            dataType: 'json',
+            success: running,
+            failure: function(){
+                alert("Error!");
+            }
+        });
+    });
+});
+
 function running(data){
-    showResult(data);
+    console.log(data);
+    showResult(JSON.parse(data));
 }
 
-function show 
+function showResult(data){
+    console.log( data.length );
+    for (var i=0; i< data.length; i++){
+       var definition = data[i];
+        console.log( i );
+    }
+}
