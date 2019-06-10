ab36d024ce7d08e86969832b5d65c8453201e4ee
annakleinhans <annakleinhans@students.berkeley.net>
2019-06-10 14:16:47 -0700

    anna finished

--- script.js
+++ script.js
@@ -15,6 +15,15 @@ $(document).ready(function(){
             }
         });
     });
+
+    $("#word").keypress(function (e) {
+        if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+            $('button[type=submit] .default').click();
+            return false;
+        } else {
+            return true;
+        }
+    });
 });
 
 function running(data){
